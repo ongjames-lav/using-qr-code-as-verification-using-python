63390511fa21b91e6bd2e8567503b765b38a6b54
ongjames-lav <142897031+ongjames-lav@users.noreply.github.com>
2025-03-25 19:31:42 +0800

    Update settings.py

--- qr_projet_1/settings.py
+++ qr_projet_1/settings.py
@@ -3,9 +3,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ongjamesdaryl@gmail.com'  # Replace with your Gmail address
-EMAIL_HOST_PASSWORD = 'cypx nggx jmwu iwiv'  # Replace with your Gmail app password
-DEFAULT_FROM_EMAIL = 'ongjamesdaryl@gmail.com'  # Replace with your Gmail address
+EMAIL_HOST_USER = ''  # Replace with your Gmail address
+EMAIL_HOST_PASSWORD = ''  # Replace with your Gmail app password
+DEFAULT_FROM_EMAIL = ''  # Replace with your Gmail address
 
 # Logging configuration
 LOGGING = {
